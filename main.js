78483f770fc1977bd3859e0b5679a5df10ba3da1
Aneesh0405 <105107683+Aneesh0405@users.noreply.github.com>
2022-10-01 16:14:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("my first console project");
+var first_name = "Aneesh"
+var last_name = "Srivastava";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+var a = 15;
+var b = 5;
+var add = a+b;
+console.log(add);
+var sub = a-b;
+console.log(sub);
+var multiply = a*b;
+console.log(multiply);
+var divide = a/b;
+console.log(divide);
